92444aa11c9790927af20285f49997a28a5e6840
brickphone <93726614+brickphone@users.noreply.github.com>
2024-06-23 13:02:09 +0200

    Fix: Footer styling fixes

--- my-app/components/footer.tsx
+++ my-app/components/footer.tsx
@@ -6,30 +6,30 @@ import Image from "next/image";
 
 const Footer = () => {
     return (
-        <div id="footer-container">
-             <div className="flex justify-center pt-12">
-                    <Separator className="w-[90%]" />
-                </div>
-            <div id="footer-inner" className="ml-[7%] pt-5 w-[90%] sm:ml-[15%]">
-                <div id="footer-top" className="pt-12 sm:flex">
-                    <div className="flex flex-col">
-                        <p className="font-medium text-3xl">Never miss any news</p>
-                        <p>We will send you weekly updates, no spam.</p>
+        <div id="footer-container" className="flex flex-col items-center pt-12">
+            <div className="w-full flex justify-center">
+                <Separator className="w-[90%]" />
+            </div>
+            <div id="footer-inner" className="w-[90%] sm:w-[45%] pt-5 flex flex-col items-center">
+                <div id="footer-top" className="pt-12 sm:flex flex-col sm:justify-center items-center w-full">
+                    <div className="flex flex-col items-center sm:items-start">
+                        <p className="font-medium text-3xl text-center sm:text-left">Never miss any news</p>
+                        <p className="text-center sm:text-left">We will send you weekly updates, no spam.</p>
                     </div>
-                    <div id="newsletter-input" className="sm:ml-[130px] flex space-x-1 mt-[12px]">
+                    <div id="newsletter-input" className="flex space-x-1 mt-[12px] sm:mt-4">
                         <Input className="w-[280px] h-[40px]" type="email" placeholder="Enter Your Email" />
                         <Button>Subscribe</Button>
                     </div>
                 </div>
-                <div id="footer-bottom" className="pt-16 sm:flex">
-                    <div id="brand" className="mb-8">
+                <div id="footer-bottom" className="pt-16 w-full flex flex-col items-center sm:flex-row sm:justify-between">
+                    <div id="brand" className="mb-8 flex flex-col items-center sm:items-start mr-12 self-start">
                         <div id="logo-name" className="flex items-center space-x-1">
                             <Image alt="logo" src={logo} width={30} height={30} />
                             <p className="font-semibold text-2xl">ThumbAI</p>
                         </div>
                         <p className="text-sm ml-1 font-light">Create beautiful thumbnails.</p>
                     </div>
-                    <div id="footer-list" className="space-y-8 flex flex-col sm:flex-row sm:ml-[50px] sm:space-y-0 space-x-0 sm:space-x-12 pt-0">
+                    <div id="footer-list" className="space-y-8 flex flex-col sm:flex-row sm:space-x-12 sm:space-y-0 w-full sm:w-auto">
                         <div id="footer-links">
                             <p className="text-xl font-semibold">Links</p>
                             <p className="text-sm">Blog</p>
@@ -47,19 +47,19 @@ const Footer = () => {
                             <p className="text-sm">Upcoming Features</p>
                         </div>
                         <div id="footer-legal">
-                            <p className="text-xl font-semibold">Legal</p>
-                            <p className="text-sm">Privacy Policy</p>
-                            <p className="text-sm">Terms Of Service</p>
-                            <p className="text-sm">Settings</p>
-                            <p className="text-sm">Cookies</p>
-                            <p className="text-sm">Contact </p>
+                        <p className="text-xl font-semibold">Legal</p>
+                        <p className="text-sm">Privacy Policy</p>
+                        <p className="text-sm">Terms Of Service</p>
+                        <p className="text-sm">Settings</p>
+                        <p className="text-sm">Cookies</p>
+                        <p className="text-sm">Contact</p>
                         </div>
                         <div id="footer-about">
-                            <p className="text-xl font-semibold">About</p>
-                            <p className="text-sm">About Us</p>
-                            <p className="text-sm">Support</p>
-                            <p className="text-sm">Settings</p>
-                            <p className="text-sm">Media</p>
+                        <p className="text-xl font-semibold">About</p>
+                        <p className="text-sm">About Us</p>
+                        <p className="text-sm">Support</p>
+                        <p className="text-sm">Settings</p>
+                        <p className="text-sm">Media</p>
                         </div>
                     </div>
                 </div>
